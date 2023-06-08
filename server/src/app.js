5d39340abe39eeb916f529773f1d059d648c2963
Kartik Patel <kartikpatel0170@gmail.com>
2023-06-08 17:48:38 -0600

    added cors lib

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require("dotenv");
 dotenv.config();
 const connect = require('./utils/connect');
@@ -7,6 +8,7 @@ const routes = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT;
 app.listen(port, async () => {
