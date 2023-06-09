84ae7f302a142dec79e5738782f6047413226603
Kartik Patel <kartikpatel0170@gmail.com>
2023-06-09 11:40:09 -0600

    added prettier + eslint config

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,14 @@ var logger = require('./config/logger');
 const routes = require('./routes/routes');
 
 const app = express();
+
+// parse json request body
 app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// enable cors
 app.use(cors());
 
 const port = process.env.PORT;
