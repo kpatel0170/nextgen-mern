9ae2e4a0a21b2dce6f61c93bcd9ada5818524baf
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-23 20:40:03 -0600

    feat: add payment services with stripe provider

---
+++ server/src/service/payment/subscription.js
@@ -0,0 +1,67 @@
+import logger from '~/utils/logger';
+import APIError from '~/utils/APIError';
+import moment from 'moment-timezone';
+import paymentProvider from '../../config/paymentProvider';
+import stripe from 'stripe';
+
+const stripe = stripe(paymentProvider.stripe.secretKey);
+
+export async function createNewSubscription(token, email, name, priceId, isTrial = false) {
+  if (!token) {
+    throw new APIError('Invalid token');
+  }
+
+  try {
+    const customer = await stripe.customers.create({
+      email,
+      name,
+      source: token,
+    });
+
+    const dataSubscription = {
+      customer: customer.id,
+      items: [{ price: priceId }],
+    };
+    if (isTrial) {
+      dataSubscription.trial_end = moment().add(14, 'd').unix();
+    }
+
+    const result = await stripe.subscriptions.create(dataSubscription);
+
+    return {
+      customer_id: customer.id,
+      subscription_id: result.id,
+    };
+  } catch (error) {
+    logger.error(error);
+    throw new APIError('Payment failed! Please check your card.');
+  }
+}
+
+export async function updateSubscription(subId, priceId) {
+  try {
+    const subscription = await stripe.subscriptions.retrieve(subId);
+
+    await stripe.subscriptions.update(subId, {
+      items: [{
+        id: subscription.items.data[0].id,
+        price: priceId,
+      }],
+    });
+
+    return true;
+  } catch (error) {
+    logger.error(error);
+    throw new APIError('Payment failed! Please check your card.');
+  }
+}
+
+export async function cancelSubscription(customerId) {
+  try {
+    await stripe.customers.del(customerId);
+    return true;
+  } catch (error) {
+    logger.error(error);
+    throw new APIError('Something went wrong!');
+  }
+}
---
+++ server/src/service/payment/webhook.js
@@ -0,0 +1,31 @@
+import { APIError } from '~/utils/APIError';
+import logger from '~/utils/logger';
+import { invoicePaymentFailed, invoicePaymentSuccess, trialWillEnd } from '../user/plans-user.service';
+
+export async function webhookStripe(req, res) {
+  let event;
+  try {
+    event = JSON.parse(req.body);
+  } catch (err) {
+    logger.error(err);
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    throw new APIError('Webhook Error', 400);
+  }
+
+  switch (event.type) {
+    case 'invoice.payment_succeeded':
+      invoicePaymentSuccess(event.data.object);
+      break;
+    case 'invoice.payment_failed':
+      invoicePaymentFailed(event.data.object);
+      break;
+    case 'customer.subscription.trial_will_end':
+      trialWillEnd(event.data.object);
+      break;
+    default:
+      logger.warn(`Unhandled event type ${event.type}`);
+      break;
+  }
+
+  res.json({ received: true });
+}
