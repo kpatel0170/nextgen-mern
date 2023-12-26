30115680ab28f1fdb6d0cd2f278dcaa8dbb70d25
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-25 22:18:16 -0600

    feat: add theme context

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { SidebarProvider } from './context/SidebarContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <SidebarProvider>
     <App />
+    </SidebarProvider>
   </React.StrictMode>
 );
