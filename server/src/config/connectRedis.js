95f6eca76e537cd7cb999285ed5913f29adf60d6
kpatel0170 <kartikpatel0170@gmail.com>
2023-12-29 23:30:42 -0600

    config: redis client (non-functioning)

---
+++ server/src/config/connectRedis.js
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+import logger from '../logger';
+
+const redisUrl = `redis://localhost:6379`;
+const redisClient = createClient({
+  url: redisUrl,
+});
+
+const connectRedis = async () => {
+  try {
+    await redisClient.connect();
+    logger.info('Redis client connected...');
+  } catch (err) {
+    logger.error(err.message);
+    setTimeout(connectRedis, 5000);
+  }
+};
+
+connectRedis();
+
+redisClient.on('error', (err) => console.log(err));
+
+export default redisClient;
